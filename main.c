aa2f94578f2674f2a35a901e3496f99726162320
José Victor <josevictorvasc@gmail.com>
2024-06-10 16:24:38 -0300

    Start

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <mpi.h>
+//CLIENT RANK SHOULD AWAYS BE 0!
+//ROOT NODE RANK SHOULD AWAYS BE 1!
+//REPLICAS RANKS SHOULD BE FROM 2->N!
+//SO IF I START WITH 8 PROCESSES, REPLICAS RANKS SHOULD BE 2->7!
+int main(int argc, char** argv) {
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+    
+    // printf("Hello, World!\n");
+    // return 0;
 }
