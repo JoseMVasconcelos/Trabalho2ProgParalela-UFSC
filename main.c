85318f2e514e760353f98d2ad23ecf4dff19ce52
JoseVasconcelos <josevictorvasc@gmail.com>
2024-06-11 20:43:35 -0300

    Inicio básico do MPI

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <mpi.h>
+
+//Feito por José Vasconcelos (22100906) e Marcos Roberto (22100915)
+//Para compilar: mpicc main.c -o main
+
 //CLIENT RANK SHOULD AWAYS BE 0!
 //ROOT NODE RANK SHOULD AWAYS BE 1!
 //REPLICAS RANKS SHOULD BE FROM 2->N!
 //SO IF I START WITH 8 PROCESSES, REPLICAS RANKS SHOULD BE 2->7!
 int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    
-    // printf("Hello, World!\n");
-    // return 0;
+    if (world_size < 3) {
+        fprintf(stderr, "World size tem que ser pelo menos 3 para %s\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    MPI_Finalize();
 }
