ba576cc96673e23716cde1a3328a967726528258
JoseVasconcelos <josevictorvasc@gmail.com>
2024-06-13 10:58:52 -0300

    Não lembro

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <mpi.h>
 
 //Feito por José Vasconcelos (22100906) e Marcos Roberto (22100915)
@@ -15,9 +16,41 @@ int main(int argc, char** argv) {
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+    //POPULAR COM MAIS TEXTO
+    char clientRequisitions[100][100] = {"Lorem Ipsum Dolo", "Teste 2", "Brabo se funcionar"};
+    int clientReqPosition = 0;
+
+
     if (world_size < 3) {
         fprintf(stderr, "World size tem que ser pelo menos 3 para %s\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    //Processo Cliente
+    if (world_rank == 0) {
+        int root_rank = 1;
+        char str[] = clientRequisitions[clientReqPosition];  
+        int stringSize = strlen(str);
+        MPI_Send(&str, stringSize, MPI_BYTE, root_rank, 0, MPI_COMM_WORLD);
+
+    }
+
+    //Processo Raiz
+    if (world_rank == 1) {
+        MPI_Status = status;
+        int client_rank = 0;
+        MPI_Rev(&stringSize, 100, MPI_BYTE, client_rank, 0, MPI_COMM_WORLD, &status);
+        MPI_Get_count(&status, MPI_BYTE, &stringSize);
+        char *token = strtok(clientRequisitions[clientReqPosition], " ");
+        while (token != NULL) {
+            printf("%s\n", token);
+            token = strtok(NULL, " ");
+        }
+    }
+
+    //Processos Réplicas
+    if (world_rank > 1) {
+        printf("%s\n", clientRequisitions[clientReqPosition+2]);
     }
 
     MPI_Finalize();
